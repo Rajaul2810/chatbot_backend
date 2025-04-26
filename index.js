cc3beddbb3cee33c66059e6b6c76f32731d97fb2
Rajaul Karim <rajaulkarim2810@gmail.com>
2025-04-26 11:36:39 +0600

    fix port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -13,7 +13,7 @@ app.use('/api/chat', chatRoute);
 
 // Define a simple route
 app.get('/', (req, res) => {
-  res.send('Hello World from Node.js!');
+  res.send('ai chatbot is running!');
 });
 
 app.listen(PORT, () => {
