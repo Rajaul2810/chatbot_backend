177e657a554e4f8b3b5760597a9fc6218923c008
Rajaul Karim <rajaulkarim2810@gmail.com>
2026-04-01 15:04:38 +0600

    add complaint status update api

--- routes/complaintRoutes.js
+++ routes/complaintRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { storeComplaint, getComplaints, deleteComplaint, updateComplaintStatus } = require('../controllers/complaintController');
+const { storeComplaint, getComplaints, updateComplaintStatus } = require('../controllers/complaintController');
 
 router.post('/', storeComplaint);
 router.get('/', getComplaints);
-router.delete('/:id', deleteComplaint);
+//router.delete('/:id', deleteComplaint);
 router.patch('/:id', updateComplaintStatus);
 
 module.exports = router;
